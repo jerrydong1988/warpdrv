c85aa717aaccf2fb6c48624ecfc53a280f9b66cb
mikjee <mik.jee@outlook.com>
2026-08-04 05:57:52 +0700

    fix backdropfilter

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  vite: {
+    build: { cssTarget: "chrome110" },
+  },
   integrations: [
     starlight({
       title: 'warpdrv',
